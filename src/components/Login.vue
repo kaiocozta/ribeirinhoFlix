d7b13459c53af60b171556393a7e6384c60a5420
Kaio Costa <kaiocostadasilva@outlook.com>
2019-04-03 21:44:50 -0400

    inicio do trabalho

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="login">
+      Componete Login
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login"
+    }
+</script>
+
+<style scoped>
+
+</style>
